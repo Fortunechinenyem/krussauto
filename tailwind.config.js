652a50fe260ea5647d3313b344b6b4af0f34ba10
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-06-23 03:18:24 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "star-sprinkles": "url('../public/star.jpg')",
+      },
+      backgroundSize: {
+        auto: "auto",
+      },
+      backgroundRepeat: {
+        repeat: "repeat",
+      },
+      fontFamily: {
+        body: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        blue1: "#F8FAFE",
+        blue: {
+          1: "#3046D9",
+        },
+        ash1: "#F8F8F8",
+        ash: "#637381",
+        ash2: "#DADCE0",
+        white: "#FFFFFF",
+        green: "#006950",
+        darkGreen: "#16B312",
       },
     },
   },
   plugins: [],
-}
+};
