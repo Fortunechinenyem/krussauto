58b15776b455a8ad1db84232b9dc98c94aafcaef
Fortunechinenyem <fortunechinenyem@gmail.com>
2023-08-21 00:46:06 +0100

    fix:deploy issues

--- pages/services/autoerrand.js
+++ pages/services/autoerrand.js
@@ -1,74 +1,74 @@
-// import React from "react";
-// import AutoErrandHero from "../../public/assets/images/kruss (1).jpg";
-// import Image from "next/image";
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-// import Link from "next/link";
+import React from "react";
+import AutoErrandHero from "../../public/assets/images/kruss (1).jpg";
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Link from "next/link";
 
-// const AutoErrand = () => {
-//   return (
-//     <div className="m-2 text-xl">
-//       <Navbar />
-//       <div className="relative h-screen bg-gradient-to-b from-gray-500 to-gray-700 w-full bg-cover">
-//         <Image
-//           className="absolute w-full h-full object-cover mix-blend-overlay"
-//           src={AutoErrandHero}
-//           alt="inspection hero"
-//           priority
-//         />
-//         <div className="absolute inset-0 bg-black opacity-50"></div>
-//         <div className="absolute inset-0 flex justify-center items-center">
-//           <div
-//             className="text-center text-white"
-//             data-aos="fade-down"
-//             data-aos-delay="400"
-//           >
-//             <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-//               Auto Arrand As A Service
-//             </h1>
-//             <p className="text-lg md:text-xl lg:text-2xl">
-//               Let us do the running around for you
-//             </p>
-//           </div>
-//         </div>
-//       </div>
+const AutoErrand = () => {
+  return (
+    <div className="m-2 text-xl">
+      <Navbar />
+      <div className="relative h-screen bg-gradient-to-b from-gray-500 to-gray-700 w-full bg-cover">
+        <Image
+          className="absolute w-full h-full object-cover mix-blend-overlay"
+          src={AutoErrandHero}
+          alt="inspection hero"
+          priority
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div
+            className="text-center text-white"
+            data-aos="fade-down"
+            data-aos-delay="400"
+          >
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
+              Auto Arrand As A Service
+            </h1>
+            <p className="text-lg md:text-xl lg:text-2xl">
+              Let us do the running around for you
+            </p>
+          </div>
+        </div>
+      </div>
 
-//       <div className="bg-white shadow-lg rounded-lg p-6 mt-9 mb-7 text-center">
-//         {/* <h3 className="text-xl md:text-xl lg:text-4xl font-bold mb-4">
-//           Auto Errand as a Service
-//         </h3> */}
-//         <p className="text-xl md:text-xl lg:text-3xl mt-4">
-//           Kruss Automotive Services offers a comprehensive Auto Errand service,
-//           designed to provide convenience and assistance with your
-//           automotive-related tasks.
-//         </p>
-//         <p className="text-xl md:text-xl lg:text-3xl mt-4">
-//           Need someone to pick up groceries, drop off packages, or handle other
-//           errands? Our dedicated team is here to help, allowing you to focus on
-//           what truly matters.
-//         </p>
-//         <p className="text-xl md:text-xl lg:text-3xl mt-4">
-//           Our reliable and professional team members ensure your errands are
-//           handled efficiently and with the utmost care, freeing up your time and
-//           reducing stress.
-//         </p>
-//         <div className="mt-9">
-//           <h3 className="text-2xl md:text-2xl lg:text-3xl text-center font-bold">
-//             Simplify Your Life with Auto Errand as a Service
-//           </h3>
-//           <div className="text-center mt-6">
-//             <Link href="/contact">
-//               <button className="inline-block bg-[#006950] text-white font-bold rounded-md px-6 py-3 text-lg">
-//                 Book Our Errand Service
-//               </button>
-//             </Link>
-//           </div>
-//         </div>
-//       </div>
+      <div className="bg-white shadow-lg rounded-lg p-6 mt-9 mb-7 text-center">
+        {/* <h3 className="text-xl md:text-xl lg:text-4xl font-bold mb-4">
+          Auto Errand as a Service
+        </h3> */}
+        <p className="text-xl md:text-xl lg:text-3xl mt-4">
+          Kruss Automotive Services offers a comprehensive Auto Errand service,
+          designed to provide convenience and assistance with your
+          automotive-related tasks.
+        </p>
+        <p className="text-xl md:text-xl lg:text-3xl mt-4">
+          Need someone to pick up groceries, drop off packages, or handle other
+          errands? Our dedicated team is here to help, allowing you to focus on
+          what truly matters.
+        </p>
+        <p className="text-xl md:text-xl lg:text-3xl mt-4">
+          Our reliable and professional team members ensure your errands are
+          handled efficiently and with the utmost care, freeing up your time and
+          reducing stress.
+        </p>
+        <div className="mt-9">
+          <h3 className="text-2xl md:text-2xl lg:text-3xl text-center font-bold">
+            Simplify Your Life with Auto Errand as a Service
+          </h3>
+          <div className="text-center mt-6">
+            <Link href="/contact">
+              <button className="inline-block bg-[#006950] text-white font-bold rounded-md px-6 py-3 text-lg">
+                Book Our Errand Service
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
 
-//       <Footer />
-//     </div>
-//   );
-// };
+      <Footer />
+    </div>
+  );
+};
 
-// export default AutoErrand;
+export default AutoErrand;
--- pages/services/cardiagnostics.js
+++ pages/services/cardiagnostics.js
@@ -1,76 +1,76 @@
-// import React from "react";
-// import CarDiagnosticsHero from "../../public/assets/images/kruss (1).jpg";
-// import Image from "next/image";
-// import Navbar from "@/components/Navbar";
-// import Footer from "@/components/Footer";
-// import Link from "next/link";
+import React from "react";
+import CarDiagnosticsHero from "../../public/assets/images/kruss (1).jpg";
+import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Link from "next/link";
 
-// const CarDiagnostics = () => {
-//   return (
-//     <div className="m-2 text-xl">
-//       <Navbar />
-//       <div className="relative h-screen bg-gradient-to-b from-gray-500 to-gray-700 w-full bg-cover">
-//         <Image
-//           className="absolute w-full h-full object-cover mix-blend-overlay"
-//           src={CarDiagnosticsHero}
-//           alt="inspection hero"
-//           priority
-//         />
-//         <div className="absolute inset-0 bg-black opacity-50"></div>
-//         <div className="absolute inset-0 flex justify-center items-center">
-//           <div
-//             className="text-center text-white"
-//             data-aos="fade-down"
-//             data-aos-delay="400"
-//           >
-//             <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-//               Expert Car Diagnostics
-//             </h1>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="mt-9 mb-7 text-xl">
-//         {/* <h3 className="text-3xl md:text-4xl lg:text-5xl text-center font-bold mb-4">
-//           Expert Car Diagnostics
-//         </h3> */}
-//         <div className="text-center md:max-w-2xl mx-auto">
-//           <p className="text-lg md:text-xl">
-//             Discover the difference with Kruss Automotive Services. Our
-//             comprehensive Car Diagnostics service goes beyond just reading error
-//             codes – our skilled technicians employ cutting-edge diagnostic tools
-//             and techniques to identify and address issues affecting your
-//             vehicle's performance.
-//           </p>
-//           <p className="text-lg md:text-xl mt-4">
-//             From warning lights to mysterious noises, we specialize in
-//             diagnosing complex problems across various systems and components.
-//             Our expertise allows us to interpret diagnostic data, analyze
-//             issues, and provide detailed insights into your car's condition.
-//           </p>
-//           <p className="text-lg md:text-xl mt-4">
-//             Our commitment is to keep your vehicle running optimally and
-//             efficiently. After a thorough diagnosis, our team will recommend
-//             targeted repairs or maintenance to resolve identified issues,
-//             ensuring your car performs at its best.
-//           </p>
-//           <p className="text-lg md:text-xl mt-4">
-//             Trust Kruss Automotive Services to deliver exceptional workmanship
-//             and exceptional customer care. Schedule a Car Diagnostics
-//             appointment today and experience the difference firsthand.
-//           </p>
-//         </div>
-//         <div className="text-center mt-6">
-//           <Link href="/contact">
-//             <button className="inline-block bg-[#006950] text-white font-bold rounded-md px-6 py-3 text-lg">
-//               Book Your Car Diagnostics Appointment
-//             </button>
-//           </Link>
-//         </div>
-//       </div>
+const CarDiagnostics = () => {
+  return (
+    <div className="m-2 text-xl">
+      <Navbar />
+      <div className="relative h-screen bg-gradient-to-b from-gray-500 to-gray-700 w-full bg-cover">
+        <Image
+          className="absolute w-full h-full object-cover mix-blend-overlay"
+          src={CarDiagnosticsHero}
+          alt="inspection hero"
+          priority
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+        <div className="absolute inset-0 flex justify-center items-center">
+          <div
+            className="text-center text-white"
+            data-aos="fade-down"
+            data-aos-delay="400"
+          >
+            <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
+              Expert Car Diagnostics
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="mt-9 mb-7 text-xl">
+        {/* <h3 className="text-3xl md:text-4xl lg:text-5xl text-center font-bold mb-4">
+          Expert Car Diagnostics
+        </h3> */}
+        <div className="text-center md:max-w-2xl mx-auto">
+          <p className="text-lg md:text-xl">
+            Discover the difference with Kruss Automotive Services. Our
+            comprehensive Car Diagnostics service goes beyond just reading error
+            codes – our skilled technicians employ cutting-edge diagnostic tools
+            and techniques to identify and address issues affecting your
+            vehicle's performance.
+          </p>
+          <p className="text-lg md:text-xl mt-4">
+            From warning lights to mysterious noises, we specialize in
+            diagnosing complex problems across various systems and components.
+            Our expertise allows us to interpret diagnostic data, analyze
+            issues, and provide detailed insights into your car's condition.
+          </p>
+          <p className="text-lg md:text-xl mt-4">
+            Our commitment is to keep your vehicle running optimally and
+            efficiently. After a thorough diagnosis, our team will recommend
+            targeted repairs or maintenance to resolve identified issues,
+            ensuring your car performs at its best.
+          </p>
+          <p className="text-lg md:text-xl mt-4">
+            Trust Kruss Automotive Services to deliver exceptional workmanship
+            and exceptional customer care. Schedule a Car Diagnostics
+            appointment today and experience the difference firsthand.
+          </p>
+        </div>
+        <div className="text-center mt-6">
+          <Link href="/contact">
+            <button className="inline-block bg-[#006950] text-white font-bold rounded-md px-6 py-3 text-lg">
+              Book Your Car Diagnostics Appointment
+            </button>
+          </Link>
+        </div>
+      </div>
 
-//       <Footer />
-//     </div>
-//   );
-// };
+      <Footer />
+    </div>
+  );
+};
 
-// export default CarDiagnostics;
+export default CarDiagnostics;
